14651e84d661006d229daf571f049f65fc9e62be
ByronFanglei <984724098@qq.com>
2024-03-07 13:33:05 +0800

    fix:修改返回首页加载问题

--- src/pages/home/ImageMeasurer/MasonryComponent.jsx
+++ src/pages/home/ImageMeasurer/MasonryComponent.jsx
@@ -10,47 +10,63 @@ export const defaultHeight = 250;
 export const defaultWidth = columnWidth;
 export const keyMapper = (item, index) => index;
 // const
-const viewportWidth = document.documentElement.clientWidth;
-const viewportHeight = document.documentElement.clientHeight;
-// Default sizes help Masonry decide how many images to batch-measure
-const cache = new CellMeasurerCache({
-  defaultHeight,
-  defaultWidth,
-  fixedWidth: true,
-});
 
-// Our masonry layout will use 3 columns with a 10px gutter between
-const cellPositionerConfig = {
-  cellMeasurerCache: cache,
-  columnCount: 6,
-  columnWidth,
-  spacer: 10,
-};
-
-const cellPositioner = createMasonryCellPositioner(cellPositionerConfig);
 const MasonryComponent = ({ itemsWithSizes, setRef, onClickItem, width }) => {
+  const viewportWidth = document.documentElement.clientWidth;
+  const viewportHeight = document.documentElement.clientHeight;
+  // Default sizes help Masonry decide how many images to batch-measure
+  const cache = new CellMeasurerCache({
+    defaultHeight,
+    defaultWidth,
+    fixedWidth: true,
+  });
+
+  // Our masonry layout will use 3 columns with a 10px gutter between
+  const cellPositionerConfig = {
+    cellMeasurerCache: cache,
+    columnCount: Math.floor(viewportWidth / columnWidth),
+    columnWidth,
+    spacer: 10,
+  };
+
+  const cellPositioner = createMasonryCellPositioner(cellPositionerConfig);
   const cellRenderer = ({ index, key, parent, style }, onClickItem) => {
     const item = itemsWithSizes?.[index]?.item;
+
     // const height = columnWidth * (size?.height / size?.width) || defaultHeight;
+    const str = Math.random().toString(36).slice(-6); //随机字符串
     return (
-      <CellMeasurer cache={cache} index={index} key={key} parent={parent}>
-        <div
-          style={style}
-          onClick={() => onClickItem(itemsWithSizes?.[index]?.item)}>
-          {/*<div>{item.id}</div>*/}
-          {item.url && (
-            <img
-              src={item.url}
-              alt={item.id}
-              style={{
-                // height: height, // 高度自定义
-                width: columnWidth,
-                display: "block",
-                borderRadius: 10,
+      <CellMeasurer
+        cache={cache}
+        index={index}
+        key={`${item?.id}-${index}-${str}`}
+        parent={parent}>
+        {({ registerChild }) => {
+          return (
+            <div
+              ref={element => {
+                if (element && registerChild) {
+                  registerChild(element);
+                }
               }}
-            />
-          )}
-        </div>
+              style={style}
+              onClick={() => onClickItem(itemsWithSizes?.[index]?.item)}>
+              {/*<div>{item.id}</div>*/}
+              {item?.url && (
+                <img
+                  src={item.url}
+                  alt={item.id}
+                  style={{
+                    // height: height, // 高度自定义
+                    width: columnWidth,
+                    display: "block",
+                    borderRadius: 10,
+                  }}
+                />
+              )}
+            </div>
+          );
+        }}
       </CellMeasurer>
     );
   };
