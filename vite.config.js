9981f46f1fbe3916cf87b807065146e9fc150dec
fengjiang <jiangfeng5@xiaomi.com>
2024-03-07 10:52:45 +0800

    feat: 引入fixReactVirtualized

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import fs from "fs";
-import { createRequire } from "node:module";
+import fixReactVirtualized from "esbuild-plugin-react-virtualized";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), reactVirtualized()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve("src"),
@@ -14,28 +13,9 @@ export default defineConfig({
       "@components": path.resolve("src/components"),
     },
   },
-});
-
-const WRONG_CODE = `import { bpfrpt_proptype_WindowScroller } from "../WindowScroller.js";`;
-export function reactVirtualized() {
-  return {
-    name: "flat:react-virtualized",
-    // Note: we cannot use the `transform` hook here
-    //       because libraries are pre-bundled in vite directly,
-    //       plugins aren't able to hack that step currently.
-    //       so instead we manually edit the file in node_modules.
-    //       all we need is to find the timing before pre-bundling.
-    configResolved() {
-      const require = createRequire(import.meta.url);
-      const file = require
-        .resolve("react-virtualized")
-        .replace(
-          path.join("dist", "commonjs", "index.jsx"),
-          path.join("dist", "es", "WindowScroller", "utils", "onScroll.js")
-        );
-      const code = fs.readFileSync(file, "utf-8");
-      const modified = code.replace(WRONG_CODE, "");
-      fs.writeFileSync(file, modified);
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [fixReactVirtualized],
     },
-  };
-}
+  },
+});
