dad667b9fe123735976d638270cc9e7c14d70c12
fanglei <fanglei5@xiaomi.com>
2024-03-07 15:44:52 +0800

    feat: resize 更新瀑布流

--- src/pages/home/ImageMeasurer/index.jsx
+++ src/pages/home/ImageMeasurer/index.jsx
@@ -1,38 +1,47 @@
-import { useRef } from "react";
+import { useRef, useImperativeHandle, forwardRef } from "react";
 import ImageMeasurer from "react-virtualized-image-measurer";
-import MasonryComponent, {
-  defaultHeight,
-  defaultWidth,
-  keyMapper,
-} from "./MasonryComponent.jsx";
-const ImageMeasurerComponent = ({ data, onClickItem }) => {
-  const setMasonry = useRef(null);
-  return (
-    <ImageMeasurer
-      items={data}
-      image={item => item.url}
-      keyMapper={keyMapper}
-      onError={(error, item, src) => {
-        console.error(
-          "Cannot load image",
-          src,
-          "for item",
-          item,
-          "error",
-          error
-        );
-      }}
-      defaultHeight={defaultHeight}
-      defaultWidth={defaultWidth}>
-      {({ itemsWithSizes }) => (
-        <MasonryComponent
-          setRef={setMasonry}
-          itemsWithSizes={itemsWithSizes}
-          onClickItem={onClickItem}
-        />
-      )}
-    </ImageMeasurer>
-  );
-};
+import MasonryComponent from "./MasonryComponent.jsx";
+import { defaultHeight, defaultWidth, keyMapper } from "./utils.js";
+const ImageMeasurerComponent = forwardRef(
+  ({ data, onClickItem, client }, ref) => {
+    const setMasonry = useRef(null);
+
+    // 使用 useImperativeHandle 来定义暴露给父组件的接口
+    useImperativeHandle(ref, () => ({
+      // 将子组件的实例传递给父组件
+      setMasonry: setMasonry.current,
+    }));
+
+    return (
+      <ImageMeasurer
+        items={data}
+        image={item => item.url}
+        keyMapper={keyMapper}
+        onError={(error, item, src) => {
+          console.error(
+            "Cannot load image",
+            src,
+            "for item",
+            item,
+            "error",
+            error
+          );
+        }}
+        defaultHeight={defaultHeight}
+        defaultWidth={defaultWidth}>
+        {({ itemsWithSizes }) => (
+          <MasonryComponent
+            client={client}
+            setRef={setMasonry}
+            itemsWithSizes={itemsWithSizes}
+            onClickItem={onClickItem}
+          />
+        )}
+      </ImageMeasurer>
+    );
+  }
+);
+
+ImageMeasurerComponent.displayName = "ImageMeasurerComponent";
 
 export default ImageMeasurerComponent;
---
+++ src/pages/home/ImageMeasurer/utils.js
@@ -0,0 +1,4 @@
+export const columnWidth = 200;
+export const defaultHeight = 250;
+export const defaultWidth = columnWidth;
+export const keyMapper = (item, index) => index;
