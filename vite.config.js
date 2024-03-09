05dbed807d90a225426fc7708a0f2eebe6a91ed2
fanglei <fanglei5@xiaomi.com>
2024-03-09 16:37:40 +0800

    feat: 移除非必要依赖，随机背景色

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import fixReactVirtualized from "esbuild-plugin-react-virtualized";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,7 +14,7 @@ export default defineConfig({
   },
   optimizeDeps: {
     esbuildOptions: {
-      plugins: [fixReactVirtualized],
+      plugins: [],
     },
   },
 });
